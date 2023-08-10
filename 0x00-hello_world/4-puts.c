189ad1b84b08fa6f1271835262d25f6ec97ac729
Endalkpan <endalkpan@gmail.com>
2023-08-10 07:40:23 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main -it prints out output using put
+ * main - A program that prints aline with puts function
  *
- * Return: always (0)
+ * Return: 0 (Success)
  *
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }       
