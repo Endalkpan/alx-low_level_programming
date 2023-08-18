16765bbba9b433593134269f561b3d0825491f2f
Endalkpan <endalkpan@gmail.com>
2023-08-18 10:38:15 -0700

    1main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,5 +7,5 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int mouth, int day, int year);
 int convert_day(int month, int day);
-
+int min(void);
 #endif
