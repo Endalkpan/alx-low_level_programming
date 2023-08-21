abe65e1833d4282bd0cf3eff0d5168d7c65eebf0
Endalkpan <endalkpan@gmail.com>
2023-08-21 08:29:36 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - prints a elements of an array of integers
+ * @n: elements parmeter input
+ * @a: string parameter input
+ * Return: Noting
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf('\n');
+}
