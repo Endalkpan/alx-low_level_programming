183349484567ddacd937557e390ec5693c4f3884
Endalkpan <endalkpan@gmail.com>
2023-08-30 08:43:22 -0700

    1 print

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,14 +2,19 @@
 
 /**
  * _print_rev_recursion - print strings in reverse
- * @s: pointer to string
- * Return: void
+ * @s: the string to reverse
+ * Return: noting
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s > '\0')
+	if (*s == '\0')
 	{
-		print_rev_recursion(s + 1);
-		_putchar(*);
+		return;
+	}
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
