ff1dd1c84e23ebe4855dd39a5ea25a8f017b18fa
Endalkpan <endalkpan@gmail.com>
2023-09-12 07:16:24 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,7 +18,7 @@ void int_dog(struct dog *d, char *name, float age, char owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
-oid free_dog(dog_t *d);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
