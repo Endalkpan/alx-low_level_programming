5872713bf02bed4c244d81aa071c438e1196922e
Endalkpan <endalkpan@gmail.com>
2024-01-12 06:03:54 -0800

    7 insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,53 +1,47 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at
- * a given position
- *
- * @h: head of the list
- * @idx: index of the new node
- * @n: value of the new node
- * Return: the address of the new node, or NULL if it failed
+ * insert_dnodeint_at_index - insets a new node
+ *	at a given position
+ * @h: the head of list
+ * @idx: the position of the new node
+ * @n: the data of the new node
+ * Return: the address of the new node
+ *	or NULL if it failed
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new;
-	dlistint_t *head;
-	unsigned int i;
+	dlistint_t *current = *h, *length = *h, *new_node;
+	unsigned int len = 0, count = 0;
 
-	new = NULL;
-	if (idx == 0)
-		new = add_dnodeint(h, n);
-	else
+	while (length)
 	{
-		head = *h;
-		if (head != NULL)
-			while (head->prev != NULL)
-				head = head->prev;
-		while (head != NULL)
-		{
-			if (i == idx)
-			{
-				if (head->next == NULL)
-					new = add_dnodeint_end(h, n);
-				else
-				{
-					new = malloc(sizeof(dlistint_t));
-					if (new != NULL)
-					{
-						new->n = n;
-						new->next = head->next;
-						new->prev = head;
-						head->next->prev = new;
-						head->next = new;
-					}
-				}
-				break;
-			}
-			head = head->next;
-			i++;
-		}
+		length = length->next;
+		len++;
+	}
+	if (len < idx)
+		return (NULL);
+	else if (len == idx)
+		return (add_dnodeint_end(h, n));
+
+	if (*h == NULL || idx == 0)
+		return (add_dnodeint(h, n));
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+
+	while (count < idx)
+	{
+		current = current->next;
+		count++;
 	}
 
-	return (new);
+	new_node->next = current;
+	new_node->prev = current->prev;
+	(current->prev)->next = new_node;
+	current->prev = new_node;
+	return (new_node);
 }
